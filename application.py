9f09e291de2ef6a74663d3ad1fbc653a941693a4
Ernesto Zhildeer Chura Flores <ernestochuraflores@gmail.com>
2022-05-08 21:03:50 -0500

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>My first Web App using Azure App Service!</h1></body></html>\n"
+    return "<html><body><h1>My first Web App using Azure App Service!</h1><br><h1>Ernesto Zhildeer Chura Flores</h1></body></html>\n"
